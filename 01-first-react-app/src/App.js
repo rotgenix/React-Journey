524c4932ce80b4631db2f72fd0bfeead1f71457b
Rohit <rohitprajapati21092003@gmail.com>
2024-02-12 16:31:20 +0530

    Created basic app with React & Vite

---
+++ 01-first-react-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hello From React App
+    </div>
+  )
+}
+
+export default App
---
+++ 02-react-vite-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hello From React-Vite App
+    </div>
+  )
+}
+
+export default App
