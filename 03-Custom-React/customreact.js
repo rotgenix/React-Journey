edc0bc346d5886df8fe877a2d59d6e3bfb2b955f
Rohit <rohitprajapati21092003@gmail.com>
2024-02-13 11:47:05 +0530

    Custom react

---
+++ 03-Custom-React/customreact.js
@@ -0,0 +1,38 @@
+
+// Render Function
+function customRender(reactElement, container) {
+    /*
+        const domElement = document.createElement(reactElement.type);
+        domElement.innerHTML = reactElement.children;
+        domElement.setAttribute('href', reactElement.props.href);
+        domElement.setAttribute('target', reactElement.props.target);
+    
+        container.appendChild(domElement);
+    */
+
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
+    container.appendChild(domElement);
+}
+
+// Element
+const reactElement = {
+    type: 'a',
+    // Props refering to attribute of a tag
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to Visit Google'
+}
+
+// Storing div 
+const mainContainer = document.querySelector("#root");
+console.log(mainContainer);
+
+// Calling custom render function
+customRender(reactElement, mainContainer);
